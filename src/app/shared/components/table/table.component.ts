17a93f36fac9446830da6f8d06b31d12dbc4ad56
hdang1612 <109531464+Hdang1612@users.noreply.github.com>
2025-06-09 09:22:53 +0700

    fix: pagination bar

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -54,28 +54,28 @@ export class TableComponent {
       for (let i = 1; i <= this.totalPages; i++) {
         pages.push(i);
       }
+    } else if (this.currentPage <= 3) {
+      // Đầu danh sách: 1 2 3 4 ... N
+      pages.push(1, 2, 3, 4, this.totalPages);
+    } else if (this.currentPage >= this.totalPages - 2) {
+      // Cuối danh sách: 1 ... N-3 N-2 N-1 N
+      pages.push(
+        1,
+        this.totalPages - 3,
+        this.totalPages - 2,
+        this.totalPages - 1,
+        this.totalPages,
+      );
     } else {
-      if (this.currentPage <= 3) {
-        pages.push(1, 2, 3, 4, this.totalPages);
-      } else if (this.currentPage >= this.totalPages - 2) {
-        pages.push(
-          1,
-          this.totalPages - 3,
-          this.totalPages - 2,
-          this.totalPages - 1,
-          this.totalPages,
-        );
-      } else {
-        pages.push(
-          1,
-          this.currentPage - 1,
-          this.currentPage,
-          this.currentPage + 1,
-          this.totalPages,
-        );
-      }
+      // Ở giữa: 1 ... P-1 P P+1 ... N
+      pages.push(
+        1,
+        this.currentPage - 1,
+        this.currentPage,
+        this.currentPage + 1,
+        this.totalPages,
+      );
     }
-    // Lọc trùng
     return Array.from(new Set(pages)).filter(
       (page) => page >= 1 && page <= this.totalPages,
     );
